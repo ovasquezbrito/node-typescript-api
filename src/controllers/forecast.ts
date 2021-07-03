04c0518bc4b14b7e0b5afec98bba5e669177e334
ovasquezbrito <ovasquezcyt@gmail.com>
2021-07-03 17:57:30 -0400

    add package pino

--- src/controllers/forecast.ts
+++ src/controllers/forecast.ts
@@ -1,28 +1,26 @@
-import { ClassMiddleware, Controller, Get } from '@overnightjs/core';
-import { authMiddleware } from '@src/middlewares/auts';
+import { Controller, Get, ClassMiddleware } from '@overnightjs/core';
+import { Request, Response } from 'express';
 import { Beach } from '@src/models/beach';
 import { Forecast } from '@src/services/forecast';
-import { Request, Response } from 'express';
+import { authMiddleware } from '@src/middlewares/auts';
+import logger from '@src/logger';
 
 const forecast = new Forecast();
 
 @Controller('forecast')
 @ClassMiddleware(authMiddleware)
 export class ForecastController {
-  /**
-   * name
-   */
   @Get('')
-  public async getForecastForLoggeUser(
+  public async getForecastForgeLoggedUser(
     req: Request,
     res: Response
   ): Promise<void> {
     try {
-      const beaches = await Beach.find({user: req.decoded?.id});
+      const beaches = await Beach.find({ user: req.decoded?.id });
       const forecastData = await forecast.processForecastForBeaches(beaches);
       res.status(200).send(forecastData);
     } catch (error) {
-      console.log(error);
+      logger.error(error);
       res.status(500).send({ error: 'Something went wrong' });
     }
   }
